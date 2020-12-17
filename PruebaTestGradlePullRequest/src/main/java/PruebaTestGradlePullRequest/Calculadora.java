9f4adfab8664bba57c499a27d87e182b1affcc85
khrixthian <cristian.fernandezro@elorrieta-errekamari.com>
2020-12-17 19:18:46 +0100

    1 commit

---
+++ PruebaTestGradlePullRequest/src/main/java/PruebaTestGradlePullRequest/Calculadora.java
@@ -0,0 +1,12 @@
+package PruebaTestGradlePullRequest;
+
+public class Calculadora {
+
+	public int suma(int x, int y) {
+		return x + y;
+	}
+
+	public int resta(int x, int y) {
+		return x - y;
+	}
+}
---
+++ PruebaTestGradlePullRequest/src/test/java/PruebaTestGradlePullRequest/TestCalculadora.java
@@ -0,0 +1,27 @@
+package PruebaTestGradlePullRequest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestCalculadora {
+	int resultado;
+	int resulesperado;
+	Calculadora calcu;
+
+	@Test
+	public void SumaTest() {
+		resultado = calcu.suma(5, 6);
+		resulesperado = 11;
+
+		assertEquals(resultado, resulesperado);
+	}
+
+	@Test
+	public void RestaTest() {
+		resultado = calcu.resta(9, 6);
+		resulesperado = 3;
+
+		assertEquals(resultado, resulesperado);
+	}
+}
